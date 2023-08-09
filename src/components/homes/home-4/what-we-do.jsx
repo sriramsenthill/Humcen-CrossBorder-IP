182908b2fb0fa574401bccf146c7f2eda11a3d9f
shaik suhail <shaiksuhail1128@gmail.com>
2023-08-09 12:25:58 +0530

    updated what we do content

--- src/components/homes/home-4/what-we-do.jsx
+++ src/components/homes/home-4/what-we-do.jsx
@@ -5,23 +5,21 @@ const tabImage = "/assets/img/feature/fea-1 copy.png";
 
 const WhatWeDo = () => {
   return (
-    <>
-      <div className="service-details-area">
-        <div className="container">
-          <div className="row service-pt-pb">
-          <div className="tp-fea-img col-12" style={{ marginLeft: "-308px" , width: "138%", height: "auto", marginTop:"-10px"}}>
-    <img src={tabImage} alt="" className='ml-110 mt-140' />
-  </div>
-            <div className="col-xl-6 col-lg-6">
-              <div className="sd-service-details-paragraph">
-                 <h3 className="tp-title-sm service-details-space center mb-0">WHAT WE DO</h3>
-                <p className="pb-15 mt-50">HumCen.io is a Blockchain-driven Cross-Border IP aggregator platform that empowers IP journey worldwide. Innovators and Businesses can get expert IP assistance, secure IP transactions, and strategic portfolio management, enabling Users to protect, manage, and monetize their novel creations on a global scale.</p>
-              </div>
-            </div>
-          </div>
-        </div>
+       <div className="container">
+        <div className="row">
+        <div className="col-xl-5 col-lg-5 wow tpfadeLeft" data-wow-duration=".5s" data-wow-delay=".5s" style={{ paddingRight: "20px" }}>
+      <div className="tp-fea-img col-12 d-none d-lg-block" style={{ marginLeft: "-308px" , width: "170%", height: "auto", marginTop:"-10px"}}>
+       <img src={tabImage} alt="" className='ml-40 mt-0 mb-30' />
+     </div>
       </div>
-    </>
+          <div className="col-xl-6 col-lg-6 ml-100 wow tpfadeRight" data-wow-duration=".5s" data-wow-delay=".7s">
+          <div className="sd-service-details-paragraph">
+                 <h3 className="tp-title-sm service-details-space center mb-0 mt-140">WHAT WE DO</h3>
+                <p className="pb-15 mt-40" style={{textAlign:"justify"}}>HumCen.io is a Blockchain-driven Cross-Border IP aggregator platform that empowers IP journey worldwide. Innovators and Businesses can get expert IP assistance, secure IP transactions, and strategic portfolio management, enabling Users to protect, manage, and monetize their novel creations on a global scale.</p>
+              </div>
+          </div>
+          </div>
+          </div>
   );
 };
 
