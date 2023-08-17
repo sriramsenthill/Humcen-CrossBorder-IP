ebcb11e3c3e217a11970a75f28051c003f7dd288
ARJUN RAHUL VIJI <avrahulkanna17@gmail.com>
2023-08-17 22:32:45 +0530

    Fixed Responsiveness for Help Services card.

--- src/components/homes/home-4/help-cards.jsx
+++ src/components/homes/home-4/help-cards.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
 
 const innovatorsList=[
     {
@@ -47,7 +50,7 @@ const ipList=[
 ]
 //col-xl-3 col-lg-4 col-md-6
 //col-12 col-md-6 col-xl-3 col-lg-6
-const HelpServices= () => {
+const HelpServices= () => {  
     return (
       <>
         <div className="ac-chose-area mb-130 ac-chose-bg2">
@@ -57,11 +60,12 @@ const HelpServices= () => {
               <h2>Our platform solves major stakeholders challenges in <br/>
               <span className='tp-title-sm'style={{fontSize:'48px',background: 'linear-gradient(270deg, #02E1B9 0%, #00ACF6 100%)',color: 'transparent',WebkitBackgroundClip: 'text',}}>Cross Border IP Industry</span></h2>
             </div>
-            <div className='d-flex m-0' style={{width:'100%'}}>
-              <ChoseItem duration='.3s' delay='.5s' icon='flaticon-group' title={<>Creators</>}
+            <div className='row' style={{width:'100%', textAlign: "center"}}>
+              <ResponsiveItem duration='.3s' delay='.5s' icon='flaticon-group' title={<>Creators</>}
                 text={'24+ Team Member'} listItems={innovatorsList} image={'/assets/img/about/innovator (1).png'}/>
-              <ChoseItem duration='.5s' delay='.7s' item_num={'tpchosebox-three'} color="fea-color-5" icon='fas fa-star' title={<>Businesses</>} text={'100% Client Satisfied'} listItems={businessList} image={'/assets/img/about/corporation.png'}/>
-              <ChoseItem duration='.7s' delay='.9s' item_num={'tpchosebox-two'} color="fea-color-4" icon='flaticon-web' title={<>IP Professionals</>} text={'65.04 k Reach'} listItems={ipList} image={'/assets/img/about/professional.png'}/>
+              <ResponsiveItem duration='.5s' delay='.7s' item_num={'tpchosebox-three'} color="fea-color-5" icon='fas fa-star' title={<>Businesses</>} text={'100% Client Satisfied'} listItems={businessList} image={'/assets/img/about/corporation.png'}/>
+              <ResponsiveItem duration='.7s' delay='.9s' item_num={'tpchosebox-two'} color="fea-color-4" icon='flaticon-web' title={<>IP Professionals</>} text={'65.04 k Reach'} listItems={ipList} image={'/assets/img/about/professional.png'}/>
+          
           </div>
           </div>
         </div>
@@ -72,30 +76,76 @@ const HelpServices= () => {
   
 export default HelpServices;
 
-const ChoseItem = ({ duration, delay, item_num,image,listItems, title, text, color }) => {
-
-    return (
-     <div className="col-12 col-md-6 col-xl-3 col-lg-6 wow tpfadeUp mr-120"
-      data-wow-duration={duration}
-     data-wow-delay={delay}>
-        <div className="tp-chose-item mb-30 d-flex justify-content-center" style={{width:'120%'}}>
-          <div className={`tpchosebox ${item_num && item_num}`} style={{height:'100%'}}>
-            <div className={`tpchosebox__icon ${color && color} mb-30`} style={{textAlign:'center'}}>
-             <img src={image} alt="" style={{width:'88px'}}/>
-            </div>
-            <div className="tpchosebox__content">
-              <h4 style={{textAlign:'center'}}>
+const ResponsiveItem = ({ duration, delay, item_num,image,listItems, title, text, color }) => {
+  return (
+    <div
+    className="col-xl-4 col-lg-6 col-md-6 col-sm-12 wow tpfadeUp mr-0"
+    data-wow-duration={duration}
+    data-wow-delay={delay}
+  >
+    <div style={{width:'100%', position: "relative"}}>
+      <div
+        className="tp-service-item-four mb-20"
+        style={{
+          height: "100%",
+          position: "relative",
+          left: "5%",
+        }}
+      >
+        <div class="tp-service-item-four__img mb-40 mt-0 bg-white ">
+          <img
+            src={image}
+            alt={title}
+            style={{width:'88px', position: "relative", left: "120%"}}
+          />
+        </div>
+        <div className="tp-service-item-four__title">
+        <h4 style={{textAlign:'center'}}>
                   <a className='tp-title-sm' style={{fontSize:'35px',cursor:'pointer',}}>{title}</a>
               </h4>
-              </div>
-              <ul>
+        </div>
+        <div className="tp-service-item-four__text" style={{textAlign:'left'}}>
+        <ul>
                 {listItems.map(item => (
              <li key={item.id} style={{ listStyle: 'disc',fontSize:'21px',margin:'15px',textAlign:'justify'}}>{item.text}</li>
             ))}
              </ul>
-            </div>
-          </div>
-          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
 
-    );
-  }
+}
+
+// const ChoseItem = ({ duration, delay, item_num,image,listItems, title, text, color }) => {
+
+//     return (
+//     <div className="container">
+//     <div className='row'>
+//      <div className="col-md-6 col-xl-3 col-lg-4 wow tpfadeUp mr-120"
+//       data-wow-duration={duration}
+//      data-wow-delay={delay}>
+//         <div className="tp-chose-item mb-30 d-flex justify-content-center" style={{width:'120%'}}>
+//           <div className={`tpchosebox ${item_num && item_num}`} style={{height:'100%'}}>
+//             <div className={`tpchosebox__icon ${color && color} mb-30`} style={{textAlign:'center'}}>
+//              <img src={image} alt="" style={{width:'88px'}}/>
+//             </div>
+//             <div className="tpchosebox__content">
+//               <h4 style={{textAlign:'center'}}>
+//                   <a className='tp-title-sm' style={{fontSize:'35px',cursor:'pointer',}}>{title}</a>
+//               </h4>
+//               </div>
+//               <ul>
+//                 {listItems.map(item => (
+//              <li key={item.id} style={{ listStyle: 'disc',fontSize:'21px',margin:'15px',textAlign:'justify'}}>{item.text}</li>
+//             ))}
+//              </ul>
+//             </div>
+//           </div>
+//           </div>
+//           </div>
+//           </div>
+
+//     );
+//   }
