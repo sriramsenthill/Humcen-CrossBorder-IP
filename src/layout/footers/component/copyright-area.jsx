3b3dab89059b6bfa9ee1e22f4d2d8edf2dd71436
ARJUN RAHUL VIJI <avrahulkanna17@gmail.com>
2023-08-24 11:15:21 +0530

    Footer fixed for Mobile Devices.

--- src/layout/footers/component/copyright-area.jsx
+++ src/layout/footers/component/copyright-area.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 const CopyrightArea = ({copy_right_text='',conditions=[],color=''}) => {
   return (
-    <div className="tp-copyright-area">
+    <div className="tp-copyright-area pb-30">
     <div className="container">
       <div className="copyright-border pt-40 wow tpfadeUp" data-wow-duration=".5s" data-wow-delay=".7s">
         <div className="row">
           <div className="col-xl-12 col-lg-12 col-12">
             <div className="tp-copyright-left text-lg-start text-start text-md-center">
               <p className={`${color?'text-black':''}`} style={{
-                textAlign: "center",
+                position: "absolute",
+                left: "50%",
+                transform: "translateX(-50%)",
               }}>{copy_right_text}</p>
             </div>
           </div>
